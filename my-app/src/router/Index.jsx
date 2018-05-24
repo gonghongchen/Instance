9d8492c8716c3d382ba99324d47e480e8afe3b15
gonghongchen_cd <gonghongchen_cd@keruyun.com>
2018-05-24 15:49:43 +0800

    remove my-app

--- my-app/src/router/Index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect, Link} from 'react-router-dom';
-
-export default props => (
-    <div>
-        This is index.
-    </div>
-);
